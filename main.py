9f9ac85c3170acd57b75705ced649b68cf1bf29f
LATIF-01 <abdellatif.sad@gmail.com>
2026-03-06 05:42:08 +0300

    more articles

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from app.services.RSS import getNews
+"""
+ARABIA=["https://english.alarabiya.net/feed/rss2/en.xml","https://english.alarabiya.net/feed/rss2/en/News.xml","https://english.alarabiya.net/feed/rss2/en/business.xml","https://english.alarabiya.net/feed/rss2/en/business/energy.xml","https://english.alarabiya.net/feed/rss2/en/life-style.xml","https://english.alarabiya.net/feed/rss2/en/webtv.xml","https://english.alarabiya.net/feed/rss2/en/in-translation.xml","https://english.alarabiya.net/feed/rss2/en/views.xml","https://english.alarabiya.net/feed/rss2/en/coronavirus.xml"]
 
-getNews("https://english.alarabiya.net/feed/rss2/en/News.xml")
+for title in ARABIA:
+    getNews(title)
+"""
+getNews("https://english.alarabiya.net/feed/rss2/en.xml")
